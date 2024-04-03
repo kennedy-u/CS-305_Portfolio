f164c7f1eda8e7d6dc4e5df19e65a9895c98401f
“Kennedy <kennedyuzoho@outlook.com>
2024-04-03 10:50:10 -0400

    refactored code base and enhanced project structure.

---
+++ CS-305_Project 2_ssl-server_student/src/test/java/com/kennedy/sslserver/SslServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kennedy.sslserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SslServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
